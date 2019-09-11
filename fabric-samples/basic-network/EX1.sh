70f7c8140b9e6be54d82cffb8c82e65e3ed60a60
hqureshiGmail <33032654+hqureshiGmail@users.noreply.github.com>
2019-09-11 18:19:58 +0500

    Update and rename start.sh to EX1.sh

--- fabric-samples/basic-network/EX1.sh
+++ fabric-samples/basic-network/EX1.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-#
-# Copyright IBM Corp All Rights Reserved
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-# Exit on first error, print all commands.
-set -ev
 
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 
 docker-compose -f docker-compose.yml down
 
+# START
 docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
 docker ps -a
 
@@ -26,5 +20,19 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/h
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
 
-docker-compose -f docker-compose.yml stop
+sleep ${FABRIC_START_TIMEOUT}
 
+# STOP
+#docker-compose -f docker-compose.yml stop
+
+# CLEAN UP
+
+# Shut down the Docker containers for the system tests.
+#docker-compose -f docker-compose.yml kill && docker-compose -f docker-compose.yml down
+
+# remove the local state
+#rm -f ~/.hfc-key-store/*
+
+# remove chaincode docker images
+#docker rm $(docker ps -aq)
+#docker rmi $(docker images dev-* -q)
