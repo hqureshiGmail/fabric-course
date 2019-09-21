894a7178ea2941574ed8993cce261c69832e3b61
hqureshiGmail <33032654+hqureshiGmail@users.noreply.github.com>
2019-09-21 15:31:14 +0500

    Update exercise-2.sh

--- chaincode/exercise-2.sh
+++ chaincode/exercise-2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash/
+#!/bin/bash
 
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n landrec -v 1.0 -p "/opt/gopath/src/github.com" -l "node"
 
