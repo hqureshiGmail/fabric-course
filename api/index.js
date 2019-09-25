0915da1571b632aaa96986397bd8de92baab8eed
Huma Qureshi <huma.qureshi@gmail.com>
2019-09-25 23:25:11 +0500

    adding invoke route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const query = require('./controllers/query')
+const invoke = require('./controllers/invoke')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/query', (req, res) => query.query(req, res))
+
+app.get('/invoke', (req, res) => invoke.invokecc(req, res))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
