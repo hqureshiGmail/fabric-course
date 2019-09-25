e975191b9ce27f01bd84581ae508239f3973eb50
Huma Qureshi <huma.qureshi@gmail.com>
2019-09-25 15:02:55 +0500

    fixinf endpoint

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const query = require('./controllers/query')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/query', (req, res) => res.send('Queried the chain'))
+app.get('/query', (req, res) => query)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
